ecee98f4244b6729320ff59a75bb11b1253320ba
Izundu Dan-Ekeh <dizundu@yahoo.com>
2025-02-03 00:29:18 +0100

    feat: update port variable value

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const numberRouter = require('./src/routes/numberRoute');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
