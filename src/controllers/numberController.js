dd8ad62b01dda3431a1a71dda0dc8ec6bb81f931
Izundu Dan-Ekeh <dizundu@yahoo.com>
2025-02-03 00:51:40 +0100

    docs: remove unnecessary comments

--- src/controllers/numberController.js
+++ src/controllers/numberController.js
@@ -9,32 +9,6 @@ const {
 const getProps = async (req, res) => {
     const number = req.query.number;
 
-    // if (
-    //     !number ||
-    //     isNaN(number) ||
-    //     !Number.isInteger(number)
-    // ) {
-    //     return res.status(400).json({ number, error: true });
-    // }
-
-    // const num = parseInt(number);
-
-    // try {
-    //     const funFactResponse = await axios.get(`http://numbersapi.com/${num}/math?json`);
-    //     const funFact = funFactResponse.data.text;
-
-    //     res.status(200).json({
-    //         number: num,
-    //         is_prime: isPrime(num),
-    //         is_perfect: isPerfect(num),
-    //         properties: classifyNumber(num),
-    //         digit_sum: digitSum(num),
-    //         fun_fact: funFact
-    //     });
-    // } catch (err) {
-    //     res.status(500).json({ error: "Internal server error" });
-    // }
-
     if (/^\d+$/.test(number)) {
         const num = parseInt(number);
 
